be45486f98bbfc4a270653c76741dd87b8da5465
Ebaa AlIbrahim <ebaaibrahime@gmail.com>
2026-06-18 14:05:09 +0300

    fix: rename server entry file

--- services/reaccommodation-service/src/index.ts
+++ services/reaccommodation-service/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://flight-reservation-ui.vercel.app',
+  origin: 'https://vercel.app',
   credentials: true
 }));
 
